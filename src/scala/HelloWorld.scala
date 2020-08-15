af51af127a1b5d5ae7c8cb381003e95cf59cf0ce
yanggit2019 <838067203@qq.com>
2020-08-15 21:53:11 +0800

    测试类型 第一天测试 第二次测试

--- src/scala/HelloWorld.scala
+++ src/scala/HelloWorld.scala
@@ -1,7 +1,19 @@
+import scala.util.control.Breaks
+
 object HelloWorld {
   def main(args: Array[String]): Unit = {
-    println("hello world")
-    println("你好")
+    println("hello,world")
+
+    val breaks = new Breaks
+    //实现break操作
+    breaks.breakable(
+      for (i <- 1 to 5){
+        if (i ==3) breaks.break()
+        print(s"${i} ")
+      }
+    )
+    
+     
   }
 
 }
