cf5009c9a4e0d8dc503ec48bbed27e6b9bf6d990
yanggit2019 <838067203@qq.com>
2020-08-15 11:16:01 +0800

    测试类型 第一天测试 第二次测试

--- src/test/java/Test2.java
+++ src/test/java/Test2.java
@@ -2,5 +2,6 @@ public class Test2 {
     public static void main(String[] args) {
         System.out.println("hello,world");
         System.out.println("第一天");
+        System.out.println("第二次测试");
     }
 }
