da0fbad27da3c09e7a8eaabaeb31158f7a39f8fe
yanggit2019 <838067203@qq.com>
2020-08-18 15:15:28 +0800

    3.创建连接对象

--- src/scala/day02/ConnecttionFactory.scala
+++ src/scala/day02/ConnecttionFactory.scala
@@ -1,8 +1,17 @@
 package day02
 
+import scala.collection.mutable.ArrayBuffer
+
 object ConnecttionFactory {
   //初始化连接
-  
+  //5.
+  private[this] val connMax =3
+  //4
+  private[this] val conns = new ArrayBuffer[Conn](connMax)
+  //6.创建连接对象放入数组
+  for(i <- 0 until connMax){
+    conns += new Conn(i)
+  }
   //提供获取总连接数
   
   //提供获取连接的操作
@@ -10,8 +19,8 @@ object ConnecttionFactory {
   //提供释放连接的操作
 
 }
-//创建连接对象
-class Conn(var id:String){
-  //相当于Java的toString方法
+//2.创建连接对象
+class Conn(var id:Int){
+  //3.相当于Java的toString方法
   override def toString: String = s"Conn[${id}]"
 }
