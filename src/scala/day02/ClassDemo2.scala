bb8b2b035fb666a429bb1a17e571ff10f57c7377
yanggit2019 <838067203@qq.com>
2020-08-18 11:02:00 +0800

    2.伴生对象可以访问

--- src/scala/day02/ClassDemo2.scala
+++ src/scala/day02/ClassDemo2.scala
@@ -2,13 +2,15 @@ package day02
 //是object ClassDemo2伴生类
 class ClassDemo2 {
   var name:String = "hainiu"
-  val age:Int = 20
-
+//  val age:Int = 20
+  private val age:Int = 20
 }
 
 //class ClassDemo2 伴生对象
 object ClassDemo2{
   def main(args: Array[String]): Unit = {
     val demo = new ClassDemo2
+    //伴生对象可以访问
+    println(demo.age)
   }
 }
