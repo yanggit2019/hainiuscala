75b871a8c4c2e2f3375f38b8c173a0d6af21d1d1
yanggit2019 <838067203@qq.com>
2020-08-18 11:25:22 +0800

    3.通过主构造器来new对象

--- src/scala/day02/ConDemo.scala
+++ src/scala/day02/ConDemo.scala
@@ -1,7 +1,15 @@
 package day02
 //定义一个主构造器
-//private修饰主构造器，只有伴生类可以通过构造器来new对象
-class ConDemo private (var name:String,val sex:String) {
+//private修饰主构造器，伴生类可以通过构造器来new对象
+class ConDemo  (var name:String,val sex:String) {
 
 }
 
+object ConDemoOther{
+  def main(args: Array[String]): Unit = {
+    //通过主构造器来new对象
+    val demo = new ConDemo("傻强", "boy")
+    demo.name="傻强2"
+    println(demo.sex)
+  }
+}
