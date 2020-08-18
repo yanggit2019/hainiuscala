0cce549eec91bf7e88d6ab97ca61236a2484c59f
yanggit2019 <838067203@qq.com>
2020-08-18 11:38:47 +0800

    3.添加val了后，加了private修饰只能伴生类才能使用

--- src/scala/day02/ConDemo2.scala
+++ src/scala/day02/ConDemo2.scala
@@ -1,6 +1,21 @@
 package day02
 //主构造器参数没有var,val修饰，代表private[this]修饰
 //最后一个参数末尾参数可以是默认参数，当new对象是可以不传
-class ConDemo2 (var name:String,addr:String = "北京市"){
+class ConDemo2 (var name:String,addr:String = "北京市",private val age:Int = 20){
 
+}
+
+object ConDemo2{
+  def main(args: Array[String]): Unit = {
+    val demo = new ConDemo2("傻强")
+    println(s"name:${demo.name},age:${demo.age}")
+//    demo.addr
+  }
+}
+//其他测试类
+object ConDemo2Other{
+  def main(args: Array[String]): Unit = {
+    val demo = new ConDemo2("傻强")
+    println(s"name:${demo.name},age:${demo.age}")
+  }
 }
