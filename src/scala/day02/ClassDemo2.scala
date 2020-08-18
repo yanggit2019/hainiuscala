2f720a93f8c89d24b2c20998c5a009de56675d0a
yanggit2019 <838067203@qq.com>
2020-08-18 11:09:00 +0800

    4.private[this]修饰的，只能本类使用

--- src/scala/day02/ClassDemo2.scala
+++ src/scala/day02/ClassDemo2.scala
@@ -3,7 +3,10 @@ package day02
 class ClassDemo2 {
   var name:String = "hainiu"
 //  val age:Int = 20
+  //private修饰的成员，当前对象和伴生对象都可访问
   private val age:Int = 20
+  //private[this]修饰的成员，只有当前类能访问，伴生对象不能访问
+  private[this] var money:Int =1
 }
 
 //class ClassDemo2 伴生对象
@@ -12,12 +15,13 @@ object ClassDemo2{
     val demo = new ClassDemo2
     //伴生对象可以访问
     println(demo.age)
+    println(demo.money)
   }
 }
 //非伴生对象
 object ClassDemo2Other{
   def main(args: Array[String]): Unit = {
     val demo = new ClassDemo2
-    demo.age
+//    demo.age
   }
 }
