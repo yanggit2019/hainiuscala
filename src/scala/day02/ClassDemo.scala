f9e0c194c2e6ea64f4e2e1182513e37a053f1c9a
yanggit2019 <838067203@qq.com>
2020-08-18 10:52:05 +0800

    3.属性值用占位符占位

--- src/scala/day02/ClassDemo.scala
+++ src/scala/day02/ClassDemo.scala
@@ -4,6 +4,11 @@ class ClassDemo {
   //1.定义不可变属性
   val sex:String ="boy"
   
+  //5.定义可变属性用下划线占位
+  //_是用来占位的，当初始化时，会根据变量类型初始化属性值，字符串初始化属性值时null
+  //当有_占位，变量类型不能省略
+  var nickName:String = _
+  
   //2.定义可变属性
   var name:String = "hainiu"
   //3.定义方法
@@ -23,5 +28,10 @@ object ClassDemoTest{
     demo.setName("hainiu123")
     //打印信息
     demo.printInfo()
+    
+    println(demo.nickName)
+    demo.nickName="大数据人员生产基地"
+    println(demo.nickName)
+   
   }
 }
