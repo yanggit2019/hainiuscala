e0bab2d89c9a6281071055b08bb34493a4aa3b3e
yanggit2019 <838067203@qq.com>
2020-08-18 14:02:57 +0800

    2.通过辅助构造器创建对象

--- src/scala/day02/ConDemo3.scala
+++ src/scala/day02/ConDemo3.scala
@@ -1,12 +1,22 @@
 package day02
 
 class ConDemo3(var name:String) {
-
+  var age:Int = _
+  //定义辅助构造器
+  def this(name:String,age:Int) = {
+    this(name)
+    this.age=age
+  }
+  
 }
 
 object ConDemo3{
   def main(args: Array[String]): Unit = {
     //通过主构造器来创建对象
     val demo = new ConDemo3("傻强")
+    //通过辅助构造器创建对象
+    val demo2 = new ConDemo3("傻强", 10)
+    println(demo2.age)
+    
   }
 }
