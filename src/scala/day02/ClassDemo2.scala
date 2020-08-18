bf1637e1d76c48c3674aaa1933d0e0f5c755abaf
yanggit2019 <838067203@qq.com>
2020-08-18 10:59:26 +0800

    1.ClassDemo2未加private

---
+++ src/scala/day02/ClassDemo2.scala
@@ -0,0 +1,14 @@
+package day02
+//是object ClassDemo2伴生类
+class ClassDemo2 {
+  var name:String = "hainiu"
+  val age:Int = 20
+
+}
+
+//class ClassDemo2 伴生对象
+object ClassDemo2{
+  def main(args: Array[String]): Unit = {
+    val demo = new ClassDemo2
+  }
+}
