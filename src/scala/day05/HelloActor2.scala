0e1ef425d913867612192c6fca80dda0d16e9fb2
yanggit2019 <838067203@qq.com>
2020-08-21 22:48:08 +0800

    1.发送同步消息

--- src/scala/day05/HelloActor2.scala
+++ src/scala/day05/HelloActor2.scala
@@ -10,6 +10,12 @@ class HelloActor2 extends Actor {
       receive{
         //5.通过模式匹配获取消息
         case "start" => println("actor receive start")
+        case "id1" => {
+          println("actor receive id1,send id2")
+          //7.返回异步无返回消息
+          sender ! "id2"
+        }
+          
       }
     }
   }
@@ -20,7 +26,13 @@ object HelloActor2{
   def main(args: Array[String]): Unit = {
     val actor = new HelloActor2
     actor.start()
-    //4.发送start消息
+    //4.发送start消息 异步无返回消息
     actor ! "start"
+    //6.发送同步消息
+    val res1 = actor !? "id1"
+    //8.强转
+    val str = res1.asInstanceOf[String]
+    //9.打印
+    println(str)
   }
 }
