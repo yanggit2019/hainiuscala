a87822974a4f2626d20a9549b317dd20a517abca
yanggit2019 <838067203@qq.com>
2020-08-21 23:13:12 +0800

    异步有返回消息

--- src/scala/day05/HelloActor2.scala
+++ src/scala/day05/HelloActor2.scala
@@ -1,6 +1,6 @@
 package day05
 
-import scala.actors.Actor
+import scala.actors.{Actor, Future}
 
 class HelloActor2 extends Actor {
   override def act(): Unit = {
@@ -17,6 +17,14 @@ class HelloActor2 extends Actor {
           //7.返回异步无返回消息
           sender ! "id2"
         }
+        case AsyncSendAndReturnMsg(id) => {
+          val num: Int = id.substring(2).toInt
+          println(s"actor receive ${id},send id${num+1}")
+          //13.加入延时
+          Thread.sleep(3000)
+          //14.返回异步无返回消息
+          sender ! AsyncSendAndReturnMsg(s"id${num+1}")
+        }
           
       }
     }
@@ -30,11 +38,25 @@ object HelloActor2{
     actor.start()
     //4.发送start消息 异步无返回消息
     actor ! "start"
-    //6.发送同步消息
+    //6.发送同步消息，等待消息返回
     val res1 = actor !? "id1"
     //8.强转
     val str = res1.asInstanceOf[String]
     //9.打印
     println(str)
+    //11.发送异步有返回消息
+    val future: Future[Any] = actor !! AsyncSendAndReturnMsg("id3")
+    //12.如果isSet为True代表有返回，如果为false代表没返回
+    future.isSet
+    //循环等待理解成在干别的活
+    while (!future.isSet){
+      //15。如果没有返回值
+      Thread.sleep(800)
+      println("sleep 800")
+    }
+    val msg2: AsyncSendAndReturnMsg = future().asInstanceOf[AsyncSendAndReturnMsg]
+    println(msg2)
   }
 }
+//13.定义样例类实现异步有返回消息的发送和接收
+case class AsyncSendAndReturnMsg(id:String)
